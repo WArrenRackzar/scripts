9ebdc887458f9bacb70fb33aaea21a1f5c8277a8
WArrenRackzar <131652951+WArrenRackzar@users.noreply.github.com>
2023-06-19 13:19:27 +0200

    Update q2.sh

--- q2.sh
+++ q2.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-input_file="your_input_file.txt"
+excluded_words=("the" "and" "in")
 
-common_words=("the" "and" "in" "is" "it" "of" "on" "to" "a" "an")
+while IFS= read -r word; do
+  word=$(echo "$word" | tr '[:upper:]' '[:lower:]')
 
-word_count=$(grep -oE '\w+' "$input_file" | tr '[:upper:]' '[:lower:]' | awk '
-  BEGIN { PROCINFO["sorted_in"] = "@val_num_desc" }
-  {
-    if (!($0 in common_words)) {
-      count[tolower($0)]++
-    }
-  }
-  END {
-    for (word in count) {
-      print word, count[word]
-    }
-  }
-')
+  if [[ ! " ${excluded_words[@]} " =~ " $word " ]] && [[ $word =~ ^[[:alpha:]]+$ ]]; then
+    word_counts[$word]=$((word_counts[$word]+1))
+  fi
+done < "$1"
 
-echo "$word_count"
+sorted_words=()
+for word in "${!word_counts[@]}"; do
+  sorted_words+=("$word ${word_counts[$word]}")
+done
+sorted_words=($(printf "%s\n" "${sorted_words[@]}" | sort -k2 -nr))
+
+for entry in "${sorted_words[@]}"; do
+  echo "$entry"
+done
